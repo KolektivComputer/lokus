139698de61e277674c3704af7609c19157ec1570
Liz Ainslie <lizzy@lizainslie.dev>
2026-08-04 19:41:44 -0500

    chore: remove ConfigType in favor of ConfigScope

--- backend/api/src/main/kotlin/capital/yuri/locus/platform/core/config/ConfigType.kt
+++
@@ -1,9 +0,0 @@
-package capital.yuri.locus.platform.core.config
-
-enum class ConfigType {
-    /** Stored at CONFIG_ROOT/<name>.jsonc */
-    Root,
-
-    /** Stored at CONFIG_ROOT/ext/<extensionId>/<name>.jsonc */
-    Extension,
-}
